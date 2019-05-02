f84166864a6bec8531e30d128db7ab62b2c38d63
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-05-02 15:28:52 -0400

    adds form to ChatForm that on user input will send the request to the backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ChatList from './ChatList';
+import axios from 'axios';
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      messages: []
+    }
+  }
+ componentDidMount() {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+  setInterval(async ()=> {
+    const {data}= await axios.get('/api') 
+    console.log(data)
+    this.setState({
+      messages: data
+    });
+   
+  }, 2000)
+}
+
+
+  //  const {data}= await axios.get('/api') 
+  //  console.log(data)
+  //  this.setState({
+  //    messages: data
+  //  });
+  // }
+  
+  render() {
+    return (
+      <div className="App">
+      <h1> Chat app</h1>
+      <ChatList messages={this.state.messages}/>
+      </div>
+    );
+  }
 }
 
 export default App;
